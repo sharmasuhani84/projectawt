2a372f1e8a2fea8f84c31429fcedede8f24edb00
sharmasuhani84 <sharmasuhani84@gmail.com>
2026-04-15 01:33:55 +0530

    fix server

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 
 // Routes
